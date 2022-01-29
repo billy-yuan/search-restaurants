a7439f34df4a23f90e3d204ffa7ed3b6f03e179c
billy-yuan <billy.yuan1@gmail.com>
2022-01-28 20:29:57 -0500

    added script that creates index + db mappings

--- scripts/add_embedding_to_db.py
+++ scripts/add_embedding_to_db.py
@@ -1,10 +1,13 @@
+from turtle import update
 from search_service.indexer import FaissIndexer
 from search_service.encoder import SbertEncoder
 import numpy as np
 from pymongo.collection import Collection
 from database.mongodb import prod_database as db
 
+# Get the number of dimensions by running a test string through the encoder
 E = SbertEncoder()
+num_dimensions = E.encode(["test"]).shape[1]
 
 
 def add_encoding_to_db(collection: Collection, text_column_name: str, embedding_column_name: str = "embedding"):
@@ -24,7 +27,7 @@ def add_encoding_to_db(collection: Collection, text_column_name: str, embedding_
         # Update record
         _id = record["_id"]
         update_details = {
-            "$set": {"embedding": embedding.tolist()}
+            "$set": {embedding_column_name: embedding.tolist()}
         }
         collection.update_one({"_id": _id}, update_details)
         i += 1
@@ -32,5 +35,55 @@ def add_encoding_to_db(collection: Collection, text_column_name: str, embedding_
     print("Finished adding encodings.")
 
 
+def add_index_to_db(collection: Collection,
+                    embedding_index_id_column_name: str = "embedding_index_id",
+                    embedding_column_name: str = "embedding"):
+    print("Adding index to db")
+
+    I = FaissIndexer()
+    # Load index if one already exists
+    I.load_index()
+    if I.index is None:
+        I.create_new_index(num_dimensions=num_dimensions)
+
+    if I.index is None:
+        raise ValueError("No index found.")
+
+    # Create mapping between index + db
+
+    # Get all records without an encoding
+    records = collection.find({embedding_index_id_column_name: {"$eq": None}})
+
+    # Get the latest index id
+    index_id: int = I.get_next_id()
+    print("Starting indexid", index_id)
+
+    i = 1
+    for record in records:
+        _id = record["_id"]
+
+        # Get the embedding from the document
+        embedding = np.array(
+            [record[embedding_column_name]]).astype(np.float32)
+        # Add embedding to index
+        I.add_to_index(embedding)
+
+        # Add index ID to the document
+        update_details = {
+            "$set": {embedding_index_id_column_name: index_id}
+        }
+        collection.update_one({"_id": _id}, update_details)
+
+        if i % 10 == 0:
+            print("{} documents added to index".format(i))
+
+        index_id += 1
+        i += 1
+
+    # Write to s3
+    I.write_to_s3()
+
+
 if __name__ == "__main__":
     add_encoding_to_db(collection=db.blurb, text_column_name="text")
+    add_index_to_db(collection=db.blurb)
