59ba49c6726369e160141bcf60ea261d35e81b54
billy-yuan <billy.yuan1@gmail.com>
2022-02-01 11:11:16 -0500

    make model min score an env variable

--- search_service/exact_search.py
+++ search_service/exact_search.py
@@ -2,13 +2,16 @@ from cgitb import text
 from typing import Any, List
 from pymongo.collection import Collection
 from database.mongodb import prod_database as db
+from search_service.s3_helpers import load_env_var
+
+min_score = 1.0*int(load_env_var("EXACT_SEARCH_MIN_SCORE"))
 
 
 class ExactSearch:
     def __init__(self, collection: Collection):
         self.collection = collection
 
-    def search(self, text_query: str, min_score: float = 3) -> List["dict[str, Any]"]:
+    def search(self, text_query: str, min_score: float = min_score) -> List["dict[str, Any]"]:
         stage_match_text = {
             '$search': {
                 'index': 'text',
